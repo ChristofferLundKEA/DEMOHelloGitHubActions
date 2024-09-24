bd06e85e0aa90001f09ece04e660a73ea49a2653
ChristofferLundKEA <chlu0002@stud.kea.dk>
2024-09-24 21:15:51 +0200

    Initial commit

---
+++ src/main/java/com/example/demohellogithubactions/DemoHelloGitHubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demohellogithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHelloGitHubActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHelloGitHubActionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demohellogithubactions/DemoHelloGitHubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demohellogithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHelloGitHubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
